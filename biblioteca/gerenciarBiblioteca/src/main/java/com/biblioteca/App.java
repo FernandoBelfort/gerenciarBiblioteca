56be563bf4c8e7c8b398e605142e5d697e0db489
Fernando Emídio Belfort do Rego <fernandoebelfortr@gmail.com>
2024-06-16 03:10:50 +0000

    parte3

--- biblioteca/gerenciarBiblioteca/src/main/java/com/biblioteca/App.java
+++ biblioteca/gerenciarBiblioteca/src/main/java/com/biblioteca/App.java
@@ -1,17 +1,41 @@
 package main.java.com.biblioteca;
 import main.java.com.biblioteca.view.MembroView;
+import java.util.UUID;
 
 public class App{
+
+    //criacao dos id simples
+    private static long currentId = 0;
+
+    public static synchronized long generateID(){
+        return ++currentId;
+    }
     
     public static void main(String[] args){
+        //criacao dos id complexos
+        UUID uniqueID1 = UUID.randomUUID();
+        UUID uniqueID2 = UUID.randomUUID();
+        UUID uniqueID3 = UUID.randomUUID();
+
+        //criacao do array lista ligada
         MembroView lista = new MembroView();
 
+        //criacao dos elementos
         lista.adiciona("Lucas");
         lista.adiciona("Pedro");
         lista.adiciona("Erik");
         lista.adiciona("Fernando");
 
+        //printagem do array com os elementos dentro
         System.out.println(lista);
+
+        //printagem dos id gerados
+        System.out.println("UUID gerado:" + uniqueID1.toString());
+        System.out.println("UUID gerado:" + uniqueID2.toString());
+        System.out.println("UUID gerado:" + uniqueID3.toString());
+        System.out.println("ID gerado:" + App.generateID());
+        System.out.println("ID gerado:" + App.generateID());
+        System.out.println("ID gerado:" + App.generateID());
     }
 }
 
